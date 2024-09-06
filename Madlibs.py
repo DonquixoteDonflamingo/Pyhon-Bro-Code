e83c9cae9138ce841c216a412c98f8e06c028576
DonquixoteDonflamingo <91290592+DonquixoteDonflamingo@users.noreply.github.com>
2024-09-06 16:57:22 -0300

    pFFFF

---
+++ Madlibs.py
@@ -0,0 +1,14 @@
+# MadLibs
+# É um jogo de palavas em que você cria a historia
+# Completando os espaços em branco
+
+adjetivo1 = input("Coloque um adjetivo (Uma descrição): ")
+substantivo1 = input("Coloque um subtântivo (Uma pessoa, lugar ou coisa): ")
+adjetivo2 = input("Coloque um adjetivo (Uma descrição): ")
+verbo = input("Coloque um verbo que termine em 'endo' (Uma ação):")
+adjetivo3 = input("Coloque um adjetivo (Uma descrição): ")
+
+print(f"Hoje eu fui a um {adjetivo1} zooologico.")
+print(f"Em uma exibição eu vi {substantivo1}")
+print(f"{substantivo1} estava {adjetivo2} é {verbo}")
+print(f"Eu estava {adjetivo3}!")
