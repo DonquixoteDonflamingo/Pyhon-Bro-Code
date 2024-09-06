67f8822ace3ff8a7906436e41a38c96bc918b524
DonquixoteDonflamingo <91290592+DonquixoteDonflamingo@users.noreply.github.com>
2024-09-06 14:24:13 -0300

    bugou aq

--- Typecasting.py
+++ Typecasting.py
@@ -1,7 +1,8 @@
 # Typecasting = O processo de conversão de uma variável de um tipo de dados para outro
 #               str(), int(), float(), bool()
+#               É util para checar se o usuário não colocou algum dado como o nome ou cpf.
 
-nome = "Gugu"
+nome = ""
 idade = 19
 nota = 4.5
 e_estudante = True
@@ -9,8 +10,12 @@ e_estudante = True
 #Você pode obter qual tipo de dados esta em uma variável usando a função type()
 # print(type(nome))
 
-nota = int(nota)
-idade = float(idade)
+#nota = int(nota)
+#idade = float(idade)
 
-print(nota)
-print(idade)
+#idade = str(idade)
+#idade += "1"
+
+nome = bool(nome)
+
+print(nome)
---
+++ UserInput.py
@@ -0,0 +1,8 @@
+# input () = Uma função que solicita ao usuário a inserção de dados
+#            Retorna os dados inseridos como uma string
+
+nome = input("Qual e o seu nome?: ")
+idade = input("Quantos anos você tem?: ")
+
+print(f"Olá {nome}!")
+print(f"Você tem {idade} anos de idade")
