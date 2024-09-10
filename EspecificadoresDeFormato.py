4d87f4f4a9ebd85093582196d52dfe2bce1a2a4e
DonquixoteDonflamingo <91290592+DonquixoteDonflamingo@users.noreply.github.com>
2024-09-10 17:01:06 -0300

    vou reiniciar

---
+++ EspecificadoresDeFormato.py
@@ -0,0 +1,12 @@
+# format specifiers = {:flags} formata um valor com base em quais sinalizadores são inseridos
+
+# .(number)f = arredondar para esse número de casas decimais (ponto fixo)
+# .(number) = alocar a quantidade escolhida de espaços
+# :03 = aloca e zera tantos espaços
+# :< = justifica à esquerda
+# :> = justifica à direita
+# :^ = se alinha com o centrão
+# :+ = usa um sinal de + para indicar que e positivo
+# := = bota um sinaal a posição mais a esquerda
+# : = coloca um espaço antes de números positivos
+# :, = bota uma virgula
