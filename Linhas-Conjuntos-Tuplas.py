735fe37ad7d649eec57333cd668fe38773a8a3e8
DonquixoteDonflamingo <91290592+DonquixoteDonflamingo@users.noreply.github.com>
2024-09-13 14:40:20 -0300

    ddddddddddd

---
+++ Linhas-Conjuntos-Tuplas.py
@@ -0,0 +1,5 @@
+# Collection (Coleção) = Única "variável" usada para armazenar vários valores
+#       Lista = [] ordanada e mutavel. Pode ter duplicados
+#       Conjunto = {} desordenada e imutavel, mas pode adicionar e mover. SEM NADA DUPLICADO
+#       Tupla = () ordenada e imutavel. Duplicados Ok. MAIS RAPIDA
+
--- loopsAninhados.py
+++ loopsAninhados.py
@@ -2,3 +2,11 @@
 #                outer loop:
 #                   inner loop:
 
+linhas = int(input("Coloque um número de linhaa: "))
+colunas = int(input("Coloque um número de colunas: "))
+simb = ""
+
+for x in range(linhas):
+    for y in range(colunas):
+        print(simb, end=" ")    # Se precisar de todos os números em uma linha só = end="" 
+    print()
